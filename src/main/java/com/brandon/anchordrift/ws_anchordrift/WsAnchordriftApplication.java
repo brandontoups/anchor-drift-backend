a9fec498a3c0dcdf66362bc6f1973d0fddc8f555
Brandon Toups <brandonmtoups@gmail.com>
2021-01-04 00:57:51 -0600

    Spring initializr

---
+++ src/main/java/com/brandon/anchordrift/ws_anchordrift/WsAnchordriftApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.anchordrift.ws_anchordrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsAnchordriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsAnchordriftApplication.class, args);
+	}
+
+}
