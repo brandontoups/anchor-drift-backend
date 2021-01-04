5e69ad5546664968add3d3d3e727cbb9316fb33a
Brandon Toups <brandonmtoups@gmail.com>
2021-01-04 01:10:18 -0600

    - rename WsAnchordriftApplication to Application - remove dumb comment in pom

---
+++ src/test/java/com/brandon/anchordrift/ws_anchordrift/Application.java
@@ -0,0 +1,13 @@
+package com.brandon.anchordrift.ws_anchordrift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsAnchordriftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
